4e1c30f5f6783975e309a065b6fac71c24e25c20
Jannis Köckritz <jannis.koeckritz@gmx.de>
2018-09-10 12:07:37 +0200

    Fix signup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to Alpha Blog, #{@user.username}"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render 'new'
 		end
